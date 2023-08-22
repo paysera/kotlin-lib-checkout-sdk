576cd7f6006fa6fbb6550901721f0b55f8dddd3d
Stanislav Georgiev <stanislav.georgiev@paysera.net>
2023-08-22 17:53:20 +0300

    Remove snapshots

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-checkout-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "1.0.1-SNAPSHOT"
+    const val version = "1.0.1"
     const val name = "Kotlin Library Checkout SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Checkout SDK Library"
